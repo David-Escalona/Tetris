17b3bd9cc7e767ba9b8fa90b38d6f64dfda19cd7
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-02-09 11:05:17 +0000

    Commit

--- piezas/modeloPieza.js
+++ piezas/modeloPieza.js
@@ -1,24 +1,24 @@
+import { models } from "./models.js"
+
 export class ModeloPieza { // Exporto una clase llamada ModeloPieza
-    constructor(modelo, x = 0, y = 0) { // Que incliye un constructor un el valor modelo, x inicializado en 0, y inicializado en 0
-        this.modelo = modelo; // Este modelo sera igual a modelo
-        this.angulo = 0; // Este angulo tendra un valor de 0
-        this.matriz = this.obtenerMatriz(); // Obtener la matriz según el modelo y el ángulo
-        this.x = x; // La x tendr el valor x que es 0
-        this.y = y; // La y tendra el valor y que es 0
-        this.longitud = this.matriz[0].length; // Longitud de la fila de la matriz
-        this.altura = this.matriz.length; // Altura de la matriz
+    constructor (modelo, x = 0, y = 0, angulo = 0) {
+      this.modelo = modelo
+      this.angulo = angulo
+      this.matriz = models[this.modelo].matriz[this.angulo]
+      this.x = x
+      this.y = y
+      this.longitud = this.matriz[0].length
+      this.altura = this.matriz.length
     }
-
-    obtenerMatriz() {
-        const modeloActual = tetrisModels[this.modelo]; // Creo una variable constante llamada modeloActual que recibe
-        return modeloActual[this.angulo % 4]; // El modeloActual me devuelve 4 posiciones de ángulo (0, 1, 2, 3)
+  
+    girar () {
+      this.angulo = this.angulo + 1
+      if (this.angulo > 3) {
+        this.angulo = 0
+      }
+      this.matriz = models[this.modelo].matriz[this.angulo]
+      this.longitud = this.matriz[0].length
+      this.altura = this.matriz.length
     }
-
-    girar() { 
-        this.angulo = (this.angulo + 1) % 4; // Actualizo la matriz según el nuevo ángulo
-        this.matriz = this.obtenerMatriz(); // Actualizo la longitud y la altura de la pieza
-        this.longitud = this.matriz[0].length;
-        this.altura = this.matriz.length;
-    }
-}
+  }
 
--- piezas/models.js
+++ piezas/models.js
@@ -1,59 +1,169 @@
-export let model = [] // Creo el array model
-
-const pieza_I = [
-        [1,1,1,1] // Defino el diseño de las piezas
-] 
-
-const pieza_Q = [
-        [1,1], // Defino el diseño de las piezas
-        [1,1]  // Defino el diseño de las piezas
-] 
-
-const pieza_T = [
-        [1,1,1], // Defino el diseño de las piezas
-        [0,1,0]  // Defino el diseño de las piezas
-] 
-
-const pieza_L = [
-        [0,0,1], // Defino el diseño de las piezas
-        [1,1,1]  // Defino el diseño de las piezas
-] 
-
-const pieza_Z = [
-        [1,1,0], // Defino el diseño de las piezas
-        [0,1,1]  // Defino el diseño de las piezas
-] 
-
-const pieza_LL = [
-        [1,0,0], // Defino el diseño de las piezas
-        [1,1,1]  // Defino el diseño de las piezas
-] 
-
-model.push(pieza_I) // Añado el diseño de piezas dentro del array model
-model.push(pieza_Q) // Añado el diseño de piezas dentro del array model
-model.push(pieza_T) // Añado el diseño de piezas dentro del array model
-model.push(pieza_L) // Añado el diseño de piezas dentro del array model
-model.push(pieza_Z) // Añado el diseño de piezas dentro del array model
-model.push(pieza_LL) // Añado el diseño de piezas dentro del array model
-
-function rotar(pieza){ // Creo una funcion llamada rotar que recibe el valor pieza
-
-        let i; // Creo una variable llamada i
-        let rotacion = [pieza]; // Creo una variable llamada rotacion que recibe el array de pieza
-
-        for(i=0;i<3;i++){ // Inicio un bucle que se incializa en 0 y se ejecutara tres veces y por cada interaccion la i se incrementa 
-                pieza = pieza[0].map((_, index) => pieza.map(row => row[index])).reverse(); // La pieza se inicializa en la primera fila mediante pieza[0]
-                // El .map((_, index)) me permite repetir los elementos de la primera fila y utilizo _ como marcador para el valor actual 
-                // El => pieza.map(row => row[index]) hace que el .map recorra las filas de pieza, y hara que cree una nueva 
-                // Y por ultimo el .reverse() hace que se invierta el orden de las filas
-                rotacion.push(pieza.map(row => [...row]));
+export const models = [
+        {
+            color: 'bg-primary',
+            nombre: 'palo',
+            matriz: [
+                [
+                    [1],
+                    [1],
+                    [1],
+                    [1]
+                ],
+                [
+                    [1, 1, 1, 1]
+                ],
+                [
+                    [1],
+                    [1],
+                    [1],
+                    [1]
+                ],
+                [
+                    [1, 1],
+                    [1, 1]
+                ]
+            ]
+        },
+        {
+            color: 'bg-secondary',
+            nombre: 'cuadrado',
+            matriz: [
+                [
+                    [1, 1],
+                    [1, 1]
+                ],
+                [
+                    [1, 1],
+                    [1, 1]
+                ],
+                [
+                    [1, 1],
+                    [1, 1]
+                ],
+                [
+                    [1, 1],
+                    [1, 1]
+                ]
+            ]
+        },
+        {
+            color: 'bg-success',
+            nombre: 'L',
+            matriz: [
+                [
+                    [1, 0],
+                    [1, 0],
+                    [1, 1]
+                ],
+                [
+                    [1, 1, 1],
+                    [1, 0, 0]
+                ],
+                [
+                    [1, 1],
+                    [0, 1],
+                    [0, 1]
+                ],
+                [
+                    [1, 1, 1],
+                    [0, 0, 1]
+                ]
+            ]
+        },
+        {
+            color: 'bg-danger',
+            nombre: 'L invertida',
+            matriz: [
+                [
+                    [0, 1],
+                    [0, 1],
+                    [1, 1]
+                ],
+                [
+                    [1, 0, 0],
+                    [1, 1, 1]
+                ],
+                [
+                    [1, 0],
+                    [1, 0],
+                    [1, 1]
+                ],
+                [
+                    [1, 1, 1],
+                    [1, 0, 0]
+                ]
+            ]
+        },
+        {
+            color: 'bg-warning',
+            nombre: 'T',
+            matriz: [
+                [
+                    [1, 1, 1],
+                    [0, 1, 0]
+                ],
+                [
+                    [1, 0],
+                    [1, 1],
+                    [1, 0]
+                ],
+                [
+                    [0, 1, 0],
+                    [1, 1, 1]
+                ],
+                [
+                    [0, 1],
+                    [1, 1],
+                    [0, 1]
+                ]
+            ]
+        },
+        {
+            color: 'bg-info',
+            nombre: 'Z',
+            matriz: [
+                [
+                    [1, 1, 0],
+                    [0, 1, 1]
+                ],
+                [
+                    [0, 1],
+                    [1, 1],
+                    [1, 0]
+                ],
+                [
+                    [1, 1, 0],
+                    [0, 1, 1]
+                ],
+                [
+                    [0, 1],
+                    [1, 1],
+                    [0, 1]
+                ]
+            ]
+        },
+        {
+            color: 'bg-dark',
+            nombre: 'Z invertida',
+            matriz: [
+                [
+                    [0, 1, 1],
+                    [1, 1, 0]
+                ],
+                [
+                    [1, 0],
+                    [1, 1],
+                    [0, 1]
+                ],
+                [
+                    [0, 1, 1],
+                    [1, 1, 0]
+                ],
+                [
+                    [1, 0],
+                    [1, 1],
+                    [0, 1]
+                ]
+            ]
         }
-        return rotacion // Devuelve el valor rotacion
-}
-
-        for(i=0;i<model.length;i++){ // Inicio un bucle que se ejecutara desde 0 hasta cada fila del model
-                model[i] = rotar(model[i]); // Se reemplaza la pieza 
-        }
-    
-    console.log(model); // Saca por consola el array model
-
+    ];
